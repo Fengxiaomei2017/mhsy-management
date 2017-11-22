0a915fe9cff427805de48a0a804d97adad3845de
fengxiaomei <fengxiaomei@mhsy.com>
2017-11-22 13:58:17 +0800

    table中的模拟数据

---
+++ src/common/js/data.js
@@ -0,0 +1,160 @@
+export function biaoti (bianma) {
+  let allbt = {
+    'Dashboard': {
+      'icon': 'icon-add_circle',
+      'bt': 'Dashboard',
+      'fbt': `General Informations and Resume`
+    },
+    'Inbox': {
+      'icon': 'icon-add_circle',
+      'bt': 'Inbox',
+      'fbt': `Your Tickets, Chats, Email`
+    },
+    'Charts': {
+      'icon': 'icon-add_circle',
+      'bt': 'Charts',
+      'fbt': `Visual Data and Statistics`
+    },
+    'UI': {
+      'icon': 'icon-add_circle',
+      'bt': 'UI',
+      'fbt': `The Basic Kit Elements`
+    },
+    'Tables': {
+      'icon': 'icon-add_circle',
+      'bt': 'Tables',
+      'fbt': `Some Rows and Columns`
+    },
+    'Forms': {
+      'icon': 'icon-add_circle',
+      'bt': 'Forms',
+      'fbt': `Some Fields and Inputs`
+    }
+  }
+  return allbt[bianma]
+}
+export function datatable () {
+  let all = [
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet Explorer 4.0',
+      'Platform': 'Win 95+',
+      'CSS grade': 'X'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet Explorer 5.0',
+      'Platform': 'Win 95+',
+      'CSS grade': 'C'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet Explorer 5.5',
+      'Platform': 'Win 95+',
+      'CSS grade': 'A'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet Explorer 5.5',
+      'Platform': 'Win XP SP2+',
+      'CSS grade': 'A'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet Explorer 5.5',
+      'Platform': 'Win XP SP2+',
+      'CSS grade': 'B'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet 5.5',
+      'Platform': 'OSX.2+',
+      'CSS grade': 'C'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Explorer 5.5',
+      'Platform': 'Win SP2+',
+      'CSS grade': 'D'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet 5.5',
+      'Platform': 'Win XP',
+      'CSS grade': 'B'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet Explorer 5.5',
+      'Platform': 'Win 98+ / OSX.2+',
+      'CSS grade': 'C'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Explorer 5.5',
+      'Platform': 'Win XP',
+      'CSS grade': 'A'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet Explorer 5.5',
+      'Platform': 'Win XP SP2+',
+      'CSS grade': 'B'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet 5.5',
+      'Platform': 'Win 98+ / OSX.2+',
+      'CSS grade': 'C'
+    },
+    {
+      'Rendering engine': 'Gecko',
+      'Browser': 'Firefox 3.0',
+      'Platform': 'Win 2k+ / OSX.3+',
+      'CSS grade': 'A'
+    },
+    {
+      'Rendering engine': 'Gecko',
+      'Browser': 'Camino 1.0',
+      'Platform': 'OSX.2+',
+      'CSS grade': 'A'
+    },
+    {
+      'Rendering engine': 'Gecko',
+      'Browser': 'Camino 1.5',
+      'Platform': 'OSX.3+',
+      'CSS grade': 'A'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet Explorer 5.5',
+      'Platform': 'Win XP SP2+',
+      'CSS grade': 'B'
+    },
+    {
+      'Rendering engine': 'Gecko',
+      'Browser': 'Firefox 3.0',
+      'Platform': 'Win 2k+ / OSX.3+',
+      'CSS grade': 'A'
+    },
+    {
+      'Rendering engine': 'Gecko',
+      'Browser': 'Camino 1.5',
+      'Platform': 'OSX.3+',
+      'CSS grade': 'A'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Internet Explorer 5.5',
+      'Platform': 'Win 98+ / OSX.2+',
+      'CSS grade': 'C'
+    },
+    {
+      'Rendering engine': 'Trident',
+      'Browser': 'Explorer 5.5',
+      'Platform': 'Win XP',
+      'CSS grade': 'A'
+    }
+  ]
+  return all
+}
