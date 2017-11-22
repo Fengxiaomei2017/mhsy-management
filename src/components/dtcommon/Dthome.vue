87084c408029288d290a3d71c3d3ca673ba9a164
fengxiaomei <fengxiaomei@mhsy.com>
2017-11-22 13:53:03 +0800

    创建home组件

---
+++ src/components/dtcommon/Dthome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Topdaohang></Topdaohang>
+    <Tubiaodaohang></Tubiaodaohang>
+    <Contentview></Contentview>
+  </div>
+</template>
+<script>
+  import Topdaohang from '@/components/dtcommon/Dttopdaohang'
+  import Tubiaodaohang from '@/components/dtcommon/Dttubiaodaohang'
+  import Contentview from '@/components/dtcommon/Dtcontent'
+  export default{
+    components: {
+      Topdaohang,
+      Tubiaodaohang,
+      Contentview
+    }
+  }
+</script>
+<style scoped>
+</style>
