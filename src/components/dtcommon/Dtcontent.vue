95deaadd43938551eb97973393016beb185ee66e
fengxiaomei <fengxiaomei@mhsy.com>
2017-11-22 13:59:45 +0800

    创建放右侧数据的组件，实现随着路由的切换，这里的数据相应的改变

---
+++ src/components/dtcommon/Dtcontent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class='content'>
+    <Zuocedaohang v-on:jiantingdh='getdhvalue'></Zuocedaohang>
+    <div class='right_content'>
+      <Contentbt :biaotishuju='biaotishuju'></Contentbt>
+      <Separaror></Separaror>
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+  import {biaoti} from '@/common/js/data.js'
+  import Zuocedaohang from '@/components/dtcommon/Dtzuocedaohang'
+  import Contentbt from '@/components/dtcontentbt/Dtcontentbt.vue'
+  import Separaror from '@/components/dtseparator/Dtseparator.vue'
+  export default {
+    data () {
+      return {
+        biaotiziduan: [
+          'Dashboard', 'Inbox', 'Charts', 'UI', 'Tables', 'Forms'
+        ],
+        biaotishuju: {}
+      }
+    },
+    methods: {
+      getdhvalue: function (index) {
+        this.biaotishuju = biaoti(this.biaotiziduan[index])
+        // console.log(this.biaotishuju)
+      }
+    },
+    components: {
+      Zuocedaohang,
+      Contentbt,
+      Separaror
+    }
+  }
+</script>
+<style scoped>
+  .content {
+    display: flex;
+    width: 80%;
+    min-width: 999px;
+    /*height: 100vh;*/
+    margin: 0 auto 20px auto;
+    overflow: hidden;
+  }
+  .content .right_content {
+    flex: 1;
+    padding: 10px;
+    color: #9D9D9D;
+    background-color: #F9F9F9;
+    border: 1px solid #DADADA;
+    box-shadow: -1px 0 1px 0 #D2D2D2;
+  }
+</style>
