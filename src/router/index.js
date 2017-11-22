b1414e889ebf95b29d86439cf7612f540a8f03f1
fengxiaomei <fengxiaomei@mhsy.com>
2017-11-22 13:56:23 +0800

    路由配置，默认路由设置，路由组件引入

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/dtcommon/Dthome'
+import Dashboard from '@/components/dtdashboard/Dtdashboard.vue'
+import Inbox from '@/components/dtinbox/Dtinbox.vue'
+import Charts from '@/components/dtcharts/Dtcharts.vue'
+import Ui from '@/components/dtui/Dtui.vue'
+import Table from '@/components/dttable/Dttable.vue'
+import Forms from '@/components/dtforms/Dtforms.vue'
 
 Vue.use(Router)
 
@@ -8,8 +14,38 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'default',
+      redirect: '/dashboard'
+    },
+    {
+      path: '/home',
+      component: Home,
+      children: [
+        {
+          path: '/dashboard',
+          component: Dashboard
+        },
+        {
+          path: '/inbox',
+          component: Inbox
+        },
+        {
+          path: '/charts',
+          component: Charts
+        },
+        {
+          path: '/ui',
+          component: Ui
+        },
+        {
+          path: '/table',
+          component: Table
+        },
+        {
+          path: '/forms',
+          component: Forms
+        }
+      ]
     }
   ]
 })
