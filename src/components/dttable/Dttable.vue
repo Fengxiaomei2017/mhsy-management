879cb3f8956a7a5009fd1ff0ec27f1e849f4e51a
fengxiaomei <fengxiaomei@mhsy.com>
2017-11-22 13:53:43 +0800

    创建table组件

---
+++ src/components/dttable/Dttable.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class='table'>
+    <Simpletable></Simpletable>
+    <Resizetable></Resizetable>
+    <Checkboxtable></Checkboxtable>
+    <Tabstable></Tabstable>
+    <Separaror></Separaror>
+    <Datatable></Datatable>
+  </div>
+</template>
+<script>
+  import Simpletable from './Dtsimpletable.vue'
+  import Resizetable from './Dtresizetable.vue'
+  import Checkboxtable from './Dtcheckboxtable.vue'
+  import Separaror from '@/components/dtseparator/Dtseparator.vue'
+  import Tabstable from './Dttabstable.vue'
+  import Datatable from './Dtdatatable.vue'
+  export default{
+    components: {
+      Simpletable,
+      Resizetable,
+      Checkboxtable,
+      Tabstable,
+      Separaror,
+      Datatable
+    }
+  }
+</script>
+<style scoped>
+</style>
