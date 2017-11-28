5cc15199a9578728718dc639bbae61a1a78c9533
fengxiaomei <fengxiaomei@mhsy.com>
2017-11-28 13:46:51 +0800

    创建路由，点击用户展示对应的曲线图

--- src/components/dtdashboard/Dtdashboard.vue
+++ src/components/dtdashboard/Dtdashboard.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>
-    dashboard页面
+  <div class='dashboard'>
+    <h3>用户列表</h3>
+    <hr/>
+    <ul>
+      <li v-for='(item,index) in yonghu'
+          class='licla'
+          title='查看血糖数据曲线图'
+          @click='chakanshuju(index)'>{{item}}</li>
+    </ul>
   </div>
 </template>
 <script>
-  export default{}
+  export default{
+    data () {
+      return {
+        yonghu: ['廖雷', '乔智兰', '杨质彬', '何春兰']
+      }
+    },
+    methods: {
+      chakanshuju: function (index) {
+        this.$router.push({path: '/quxiantu', query: {index: index}})
+      }
+    }
+  }
 </script>
 <style scoped>
-
+  .dashboard {
+    margin: 0 10px;
+  }
+  hr {
+    margin: 10px 0;
+    border: 0.5px solid #DEDBDB;
+  }
+  .dashboard .licla {
+    height: 30px;
+    line-height: 30px;
+    cursor: pointer;
+  }
 </style>
