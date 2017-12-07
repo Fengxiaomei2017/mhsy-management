4abac59155686d2ae032e7194b612ceaade17ef5
fengxiaomei <fengxiaomei@mhsy.com>
2017-12-07 15:58:35 +0800

    写入form列表种类，点击对应种类，传入对应的值路由到页面

--- src/components/dtdashboard/Dtdashboard.vue
+++ src/components/dtdashboard/Dtdashboard.vue
@@ -8,18 +8,28 @@
           title='查看血糖数据曲线图'
           @click='chakanshuju(index)'>{{item}}</li>
     </ul>
+    <hr />
+    <ul>
+      <li class='licla'
+          v-for='(item,index) in shuju'
+          @click='chakanbiaodan(index)'>{{item}}</li>
+    </ul>
   </div>
 </template>
 <script>
   export default{
     data () {
       return {
-        yonghu: ['廖雷', '乔智兰', '杨质彬', '何春兰']
+        yonghu: ['廖雷', '乔智兰', '杨质彬', '何春兰'],
+        shuju: ['用户信息', '用户', '音频数据', '微信账号', '微信用户', '设备状态', '设备用户', '设备微信', '设备', '日常数据']
       }
     },
     methods: {
       chakanshuju: function (index) {
         this.$router.push({path: '/quxiantu', query: {index: index}})
+      },
+      chakanbiaodan: function (index) {
+        this.$router.push({path: '/yonghuxinxi', query: {index: index}})
       }
     }
   }
